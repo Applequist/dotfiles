45ef3dc3066548393e9992b66f91fbf406557f6d
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-11-05 12:14:05 +0300

    Add Neotree plugin to the ui topic.

--- .config/nvim/lua/plugins/topics/ui.lua
+++ .config/nvim/lua/plugins/topics/ui.lua
@@ -1,5 +1,7 @@
 return {
   { "nvim-tree/nvim-web-devicons" },
+  { "MunifTanjim/nui.nvim", lazy = true },
+
   -- Status line
   {
     "nvim-lualine/lualine.nvim",
@@ -32,5 +34,77 @@ return {
       }
 
     end,
-  }
+  },
+
+  -- Neotree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    branch = "v3.x",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "[F]ile [E]xplorer (cwd)"
+      },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ toggle = true, source = "buffers" })
+        end,
+        desc = "[B]uffer [E]xplorer",
+      },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          -- expander_collapsed = "-",
+          -- expander_expanded = "+",
+          expander_highlight = "NeoTreeExpander"
+        },
+      },
+    },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
+  },
 }
