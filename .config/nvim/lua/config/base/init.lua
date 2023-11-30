70803774ea787daf8f67e84e2ec5f9a1b6d1f7f9
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-11-30 14:55:17 +0300

    Move to LazyVim... again

--- .config/nvim/lua/config/base/init.lua
+++
@@ -1,2 +0,0 @@
-require("config.base.options")
-require("config.base.keymaps")
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,34 +1,46 @@
--- Bootstrap lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
+  -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
--- Set basic options and keymaps
-require("config.base")
-
--- Load plugins.
--- Plugins are split in 2 broad sub-categories:
---  * base: plugins that provide common dependencies and/or general enhancements to neovim and
---          that do not have an obvious place in one of the sub-topics below;
---  * topics: these plugins are further split into sub-topics:
---    * ui: plugins providing general UI enhancements outside the buffers, eg lualine for statusline,
---          neotree for file tree navigation (and more)...
---    * editor: for general text editing enhancements, eg navigation in and between files, eg telescope, treesitter...
---    * lsp: for general LSP server configurations
---    * cmp: for general completion configuration, eg nvim-cmmp, luasnip...
---    * coding: for general coding tools configuraion, eg test runner, debugging...
---    * <lang>: per language specific configuration
 require("lazy").setup({
-  { import = "plugins.base" },
-  { import = "plugins.topics" },
-}, {
-  install = {
-    colorscheme = { "onenord" },
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
-
-
